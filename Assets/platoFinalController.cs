78e300ef55777d5d3e8dd1cf96bd55ea26c8888b
Valehica <valehica@gmail.com>
2024-07-04 00:13:00 -0400

    platofinal

---
+++ Assets/platoFinalController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platoFinalController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int type;
+    public bool toco;
+    public  float perfeccionArrozP1;
+    public  float perfeccionCarneP1;
+    public  float perfeccionArrozP2;
+    public  float perfeccionCarneP2;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "plato")
+        {
+            if (type == 1)
+            {   
+                perfeccionArrozP1 = other.GetComponentInParent<platoController>().perfeccionArroz;
+                perfeccionCarneP1 = other.GetComponentInParent<platoController>().perfeccionCarne;
+            }
+            else if (type == 2) {
+                perfeccionArrozP2 = other.GetComponentInParent<platoController>().perfeccionArroz;
+                perfeccionCarneP2 = other.GetComponentInParent<platoController>().perfeccionCarne;
+            }
+            toco = true;
+        }
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
